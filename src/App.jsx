4e8ee09f6b66d6edb068ba786027af99e95d5dd2
unknown <luthandodidiza197@gmail.com>
2023-06-08 11:24:27 +0200

    added height

--- src/App.jsx
+++ src/App.jsx
@@ -25,10 +25,10 @@ function App() {
 
   return (
     <>
-    <div className='w-full h-full absolute top-0 bg-slate-800'>
-      <header className='w-full relative h-40 bg-black'>
+    <div className='w-full  min-h-screen  bg-slate-800'>
+      <header className='w-full py-12'>
 
-      <h2 className='text-2xl text-white text-center pt-20 tracking-[7px]'><span className='text-pink-800 font-bold'>to</span><span className='text-sky-800 font-bold'>do</span><span className='text-emerald-800 font-bold'> li</span><span className='text-orange-800 font-bold'>st</span></h2>
+      <h2 className='text-2xl my-8 text-white text-center pt-20 tracking-[7px]'><span className='text-pink-800 font-bold'>to</span><span className='text-sky-800 font-bold'>do</span><span className='text-emerald-800 font-bold'> li</span><span className='text-orange-800 font-bold'>st</span></h2>
 
       <div className='w-full flex justify-center absolute -bottom-8'>
       <input className='rounded-xl focus:border-none focus:border-orange-500 focus:border py-5 px-10 lg:w-[30rem] w-[15rem] sm:w-[19rem]' placeholder='Enter your todo item to list..... ' value={name} onChange={(e)=> setName(e.target.value)}  />
@@ -38,13 +38,13 @@ function App() {
       </div>
 
 
-      <div className="mt-36 flex justify-center w-full">
+      <div className="mt-[5rem] flex justify-center w-full">
         <ul className="flex flex-col gap-8">
         {todos && todos.map((item)=>(
           <li key={item.id} className='rounded-xl bg-yellow-800 shadow-lg  flex items-center justify-between py-3 px-10 lg:w-[30rem] w-[15rem] sm:w-[19rem]'>
             <span onClick={()=>toggle(item.id)} className={`rounded-full p-2 border-2 ${item.isCompleted && 'bg-red-500'} border-red-600 mr-4`}></span>
             <span className={`text-xl tracking-[4px] ${item.isCompleted && 'line-through'} font-semibold text-emerald-400`}>{item.task}</span>
-            <BsTrash className='text-2xl font-bold' onClick={() => setTodos(todos.filter(todo=>todo.name !== item.task))}/>
+            <BsTrash className='text-2xl font-bold' onClick={() => setTodos(todos.filter(todo=>todo.id !== item.id))}/>
           </li>
         ))}
           
